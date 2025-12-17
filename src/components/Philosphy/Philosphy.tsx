c2a57f3e524d713db994d7b30e141ea308122638
Porvik.sh <165302978+porvik-sh@users.noreply.github.com>
2025-12-17 07:28:50 +0530

    Add Philosphy component with layout and content

--- src/components/Philosphy/Philosphy.tsx
+++ src/components/Philosphy/Philosphy.tsx
@@ -1,1 +1,53 @@
+import React from "react";
+import { TbSend } from "react-icons/tb";
+import Image from "next/image";
+import { Link } from "react-scroll";
+import avatar from "../../assets/avatar3.png";
+
+
+const Philosphy = () => {
+  return (
+    <section
+      id="#Philosphypage" className="w-full h-[100vh] py-10 items-center justify-center md:p-0 text-center lg:gap-5 gap-5 md:flex">
+      {" "}
+      <div className="flex flex-col items-center justify-center space-y-10 md:space-y-3">
+        <div className="flex gap-2 text-4xl">
+        </div>
+        <div className="flex items-center gap-2 font-bold text-center">
+          <hr className="w-[70px]" />
+          <p id="job-title" className="text-xs text-black">BurritoBowl Philosphy</p>
+          <hr className="w-[70px]" />
+        </div>
+        <p className="text-sm font-normal md:w-[450px] text-center w-[300px] ">
+          <span className="font-bold">BurritoBowl</span> is a fundamental sense of achieving -extraordinary via Domain Mix (STEM -science,technology,education,Math ofcrs) not gonna tell it's a bendable excercise for Mind Expansion.Personally, an addiction rather then a sense of satiety~
+          
+        </p>
+        <Link
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-150}
+          duration={500}
+        >
+          <button data-testid="contact-me" className="flex items-center justify-center gap-2 p-2 px-4 py-2 text-xs text-white duration-300 bg-black rounded hover:opacity-80 group" aria-label="Contact Me">
+            <p>interact.</p>
+            <TbSend className="group-hover:rotate-45" />
+          </button>
+        </Link>
+      </div>
+      <div
+        className="relative w-[400px] h-[380px] rounded-md mx-auto my-5 md:mx-10">
+        <Image
+          src={avatar}
+          loading="lazy"
+          alt="avatar-home"
+          fill
+          className="rounded-[50%] object-cover"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Philosphy;
 
