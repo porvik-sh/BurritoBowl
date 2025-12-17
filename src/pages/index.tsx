379e31f095738262ba9339aedc642beb0af1afbd
Porvik.sh <165302978+porvik-sh@users.noreply.github.com>
2025-12-17 07:33:32 +0530

    Update Home component to include MedSkills and Latestworks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,22 @@
 import Navbar from "@/components/Navbar/Navbar";
-import About from "./../components/About/About";
+import Latestworks from "./../components/Latestworks/Latestworks";
 import Contact from "./../components/Contact/Contact";
 import Footer from "./../components/Footer/Footer";
 import Hero from "./../components/Hero/Hero";
 import Journey from "./../components/Journey/Journey";
-import Projects from "./../components/Projects/Projects";
+//import Projects from "./../components/Projects/Projects";
 import Skills from "./../components/Skills/Skills";
-
+import MedSkills from "./../components/Medicine/MedSkills";
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
-      <About />
-      <Skills />
       <Journey />
-      <Projects />
+      <MedSkills />
+      {/*<Projects />*/}
+      <Skills />
+      <Latestworks />
       <Contact />
       <Footer />
     </>
